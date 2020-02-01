5140df2a1bb66507176a5e30d7f62b7bf9fb11fa
gaa <gaa@192.168.0.34>
2020-02-01 19:24:08 +0100

    Diagrama UML creado.

--- PiedraPapelTijera/src/MoveWords.java
+++ PiedraPapelTijera/src/MoveWords.java
@@ -7,7 +7,7 @@ public class MoveWords{
     public static final int PIERDE = 2;
 
     private static final String[] validMoves = {"TIJERAS", "PAPEL", "PIEDRA", "LAGARTIJA", "SPOCK"};
-    private static final String[] validCommands = {"SALIR", "HELP"};
+    private static final String[] validCommands = {"SALIR", "HELP"}; 
 
     private Random rnd;
     
